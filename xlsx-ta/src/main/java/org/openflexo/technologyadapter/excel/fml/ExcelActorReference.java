287ec3b1c5a6288bea6408d98fcb498be81d64ec
Sylvain Guérin <sylvain.guerin@imt-atlantique.fr>
2025-02-07 09:27:45 +0100

    MEDIUM / Migrate ModelSlotInstance API change

--- xlsx-ta/src/main/java/org/openflexo/technologyadapter/excel/fml/ExcelActorReference.java
+++ xlsx-ta/src/main/java/org/openflexo/technologyadapter/excel/fml/ExcelActorReference.java
@@ -44,7 +44,7 @@ import java.util.logging.Logger;
 import org.openflexo.foundation.FlexoException;
 import org.openflexo.foundation.fml.annotations.FML;
 import org.openflexo.foundation.fml.rt.ActorReference;
-import org.openflexo.foundation.fml.rt.ModelSlotInstance;
+import org.openflexo.foundation.fml.rt.FreeModelSlotInstance;
 import org.openflexo.foundation.resource.ResourceLoadingCancelledException;
 import org.openflexo.logging.FlexoLogger;
 import org.openflexo.pamela.annotations.Getter;
@@ -113,7 +113,7 @@ public interface ExcelActorReference<T extends ExcelObject> extends ActorReferen
 		}
 
 		public ExcelWorkbookResource getExcelWorkbookResource() {
-			ModelSlotInstance<?, ?> msInstance = getModelSlotInstance();
+			FreeModelSlotInstance<?, ?> msInstance = (FreeModelSlotInstance<?, ?>) getModelSlotInstance();
 			if (msInstance != null && msInstance.getResource() instanceof ExcelWorkbookResource) {
 				return (ExcelWorkbookResource) msInstance.getResource();
 			}
